0459e9bf4a47af64f1767749b284594de874d857
Muhammad Azhar <160399289+muhammadazhar17@users.noreply.github.com>
2025-02-06 00:09:14 +0500

    Update page.tsx

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,5 +1,6 @@
 /**
- * This route is responsible for the built-in authoring environment using Sanity Studio.
+ * This route is responsible for the built-in authoring environment using Sanity npm run build
+ * .
  * All routes under your studio path is handled by this file using Next.js' catch-all routes:
  * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
  *
@@ -8,7 +9,7 @@
  */
 
 import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
+import config from '../../../../sanity.config'
 
 export const dynamic = 'force-static'
 
